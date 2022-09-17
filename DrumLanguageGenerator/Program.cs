a388864c1cb600496a6f3dabb70bafe2862136ea
nijikuu <visualproducerofficial@outlook.com>
2022-09-17 23:31:07 +0800

    添加项目文件。

---
+++ DrumLanguageGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+while (true)
+{
+    string dlg=File.ReadAllText(@"./drumlanguages.txt");
+    JArray arr=JsonConvert.DeserializeObject<JArray>(dlg);
+    List<string> list = new();
+    foreach (var item in arr)
+    {
+        list.Add(item.ToString());
+    }
+    Console.WriteLine("This program does not have an exception handling mechanism, the program crash is not responsible");
+    Console.WriteLine($"Please choose a template(0-{list.Count-1})");
+    int index = int.Parse(Console.ReadLine().Replace("\n", "").Replace("\r", ""));
+    Console.WriteLine("Please enter an overview of the events(use \\n to line break)");
+    string sum=Console.ReadLine().Replace("\n","").Replace("\r","").Replace(@"\n","\n");
+    Console.WriteLine("Please enter the whole thing(use \\n to line break)");
+    string all = Console.ReadLine().Replace("\n", "").Replace("\r", "").Replace(@"\n", "\n");
+    Console.WriteLine("Please enter name of the person who you want to persecute(use \\n to line break)");
+    string name = Console.ReadLine().Replace("\n", "").Replace("\r", "").Replace(@"\n", "\n");
+    string result = list[index].Replace("%name%", name).Replace("%example_sum%",sum).Replace("%example_main%",all);
+    Console.WriteLine(result);
+    Console.ReadKey();
+}
